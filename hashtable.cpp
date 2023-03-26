bfc1b84cbda444a2e1a4907a62b3c526af8d5753
nhatanh2709 <22520059@gm.uit.du.vn>
2023-03-26 21:05:28 +0700

    init project

---
+++ hashtable.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int MOD=1e5+7;
+const int base=31;
+vector<string>block[MOD+10];
+int get_hash(string s)
+{
+    int ans=0;
+    for(char c:s)
+    {
+        int value=c-'0';
+        ans=(ans*base+value);
+    }
+    return ans;
+}
+void find_string(string s)
+{
+    int value=get_hash(s);
+    bool ok=true;
+    for(string need:block[value])
+    {
+        if(need==s)ok=false;
+    }
+    if(ok)block[value].push_back(s);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        find_string(s);
+    }
+}
+
